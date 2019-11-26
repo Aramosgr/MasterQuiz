71da63f76a3c88e562fea654ba265094a35a38c1
Aramosgr <aramosgr@gmail.com>
2019-11-26 20:55:38 +0000

    Adding multilangue
    Using the library react-intl

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import SideDrawer from './components/Navigation/SideDrawer/SideDrawer';
 
 function App() {
   return (
     <div className="App">
+      <SideDrawer/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { IntlProvider } from "react-intl";
+import messages_en from "./assets/translations/en.json";
+import messages_es from "./assets/translations/es.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const messages = {
+    'es': messages_es,
+    'en': messages_en
+};
+
+const language = navigator.language.split(/[-_]/)[0]; 
+
+ReactDOM.render(
+    <IntlProvider locale={language} messages={messages["es"]}>
+        <App />
+    </IntlProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
