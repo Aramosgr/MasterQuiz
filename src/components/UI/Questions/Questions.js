94f6a7279a8a20e429559a601049b6cd6b56f21c
Aaron Ramos <aramos@abacuscorp.local>
2019-11-27 10:40:14 +0000

    UI questions
    one more dummy UI section

---
+++ src/components/UI/Questions/Questions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Auxiliar from '../../../hoc/Auxiliar';
+
+const Questions = (props) => {
+    return (
+        <Auxiliar>
+            Im the questions
+        </Auxiliar>
+    )
+}
+
+export default Questions;
